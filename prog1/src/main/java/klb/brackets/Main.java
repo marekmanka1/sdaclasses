0809b6a55c29cac7a53cd6080a7ae0fd4fbc1344
Marek Manka <marek_ma@wp.pl>
2018-03-18 15:24:29 +0100

    third commit

--- prog1/src/main/java/klb/brackets/Main.java
+++ prog1/src/main/java/klb/brackets/Main.java
@@ -4,7 +4,7 @@ package klb.brackets;
 public class Main {
 
     public static void main(String[] args) {
-        String S = "(M[arek M.)";
+        String S = "(M[arek M.){hh[jk][h]}";
 
         Brackets myBrackets = new Brackets();
         myBrackets.solution(S);
